16ba337dde6d010562ec45550cc82186cbb38f79
arongrk <aron.grabski@gmail.com>
2022-12-27 15:55:01 +0100

    test

---
+++ tastaturtest.py
@@ -0,0 +1,10 @@
+a = int(input('Please enter an integer: '))
+b = int(input('Please enter another integer: '))
+
+c = a + b
+
+print(f'The Sum of your integers is {c}')
+
+
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ
+# abcdefghijklmnopqrstuvwxyz
